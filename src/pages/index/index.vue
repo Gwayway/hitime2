5a566a689f3a440300e83b04e21a28a05c176cb0
chuwill <952698119@qq.com>
2022-01-09 10:34:12 +0800

    add TDesign

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,21 +1,27 @@
 <template>
   <div>
     <!-- 头部icon,text -->
-    <i-row i-class="top-row">
-      <i-col span="6"
-        ><i-avatar i-class="icon" src="/static/images/disk.png" shape="square" size="large"></i-avatar
-      ></i-col>
-      <i-col><label class="text">Hitime</label></i-col>
-    </i-row>
+    <wux-row wux-class="top-row">
+      <wux-col span="3"
+        ><wux-avatar
+          wux-class="icon"
+          src="/static/images/disk.png"
+          shape="square"
+          size="large"
+      /></wux-col>
+      <wux-col><label class="text">Hitime</label></wux-col>
+    </wux-row>
+
     <!-- 中部动画 -->
     <div class="div-pic">
-      <img class="pic" src="/static/images/cat.png"/>
+      <img class="pic" src="/static/images/cat.png" />
     </div>
-    <div class="div-button">
-      <i-button i-class="button" type="primary" @click="clickHandle"
-        >进入你的Hi!Time</i-button
+    <div class="div">
+      <button class="button" type="primary" @click="clickHandle"
+        >进入你的Hi!Time</button
       >
     </div>
+      
   </div>
 </template>
 
@@ -48,25 +54,40 @@ export default {
 </script>
 
 <style scoped>
-.div-button{
+.div{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
   width: 100%;
+  height: 100%;
 }
-.pic{
+.div-button {
+  margin: 0 auto;
+  display:flex;
+  align-items: center;
+}
+.pic {
   margin-top: 100rpx;
 }
-.div-pic{
+.div-pic {
   width: 100%;
   height: 60%;
-  text-align:center;
+  text-align: center;
 }
-div >>> .button {
-  width: 350px;
-  border-radius: 10px !important;
-  background-color: #6236ff !important;
+.button {
+  display: inline;
+  justify-content: center;
+  width: 350px ;
+  border-radius: 10px ;
+  background-color: #6236ff;
   position: fixed;
   bottom: 200rpx;
+  
+  
 }
 div >>> .top-row {
+  flex: initial;
   margin: 60px 20px;
 }
 
